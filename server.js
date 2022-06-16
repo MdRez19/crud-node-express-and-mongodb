067fcd56ad399f2b344141e0a72b23a53484f4fa
rezaul choudhury <rofy2009@gmail.com>
2022-06-16 10:01:39 +0100

    casts.ejs file created

--- server.js
+++ server.js
@@ -11,20 +11,31 @@ MongoClient.connect(url, (error, client) => {
             console.log('Connected to MongoDB')
             const db = client.db('star-wars-quotes')
             const quotesCollection = db.collection('quotes')
+            const castsCollection = db.collection('casts')
             app.set('view engine', 'ejs');
             app.use(bodyParser.urlencoded({extended: true}));
             app.get('/', (request, response) => {
                 quotesCollection.find().toArray()
+                castsCollection.find().toArray()
                     .then(results => {
                         console.log(results)
-                        response.render('index.ejs',  { quotes: results
-                        })
+                        response.render('index.ejs',  { quotes: results })
+                        response.render('casts.ejs',  { casts: results })
                     })
                     .catch(error => console.error(error))
             })
             app.post('/quotes', (request, response) => {
                 console.log(request)
                 quotesCollection.insertOne(request.body)
+                    .then(result => {
+                        console.log(result)
+                        response.redirect('/')
+                    })
+                    .catch(error => console.error(error))
+            })
+            app.post('/casts', (request, response) => {
+                console.log(request)
+                castsCollection.insertOne(request.body)
                     .then(result => {
                         console.log(result)
                         response.redirect('/')
